aff2ae049b4655d1e9b83d0363968a25cc64cd07
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-17 14:02:52 +0100

    closing menu after clicking outside of it

--- bottom-menu/script.js
+++ bottom-menu/script.js
@@ -8,3 +8,13 @@ openMenuButtons.forEach((btn) => {
 		document.querySelector('.menu').style.translate = `-${btn.getAttribute('data-menu-num') * 25}% 0%`
 	})
 })
+
+window.addEventListener("click", function (e) {
+  if (
+    !document.querySelector('.nav').contains(e.target) &&
+    !document.querySelector('.menu').contains(e.target)
+  ) {
+    // if user clicks outside menu close it
+    document.querySelector(".menu").classList.remove("active");
+  }
+});
