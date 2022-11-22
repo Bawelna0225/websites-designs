ebdc4da8190f3899a1566148089db847dcea42ee
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-22 15:36:26 +0100

    darkmode btn & darkmode

--- bottom-menu/script.js
+++ bottom-menu/script.js
@@ -8,12 +8,40 @@ openMenuButtons.forEach((btn) => {
 	})
 })
 
-window.addEventListener("click", function (e) {
-  if (
-    !document.querySelector('.nav').contains(e.target) &&
-    !document.querySelector('.menu').contains(e.target)
-  ) {
-    // if user clicks outside menu close it
-    document.querySelector(".menu").classList.remove("active");
-  }
-});
+window.addEventListener('click', function (e) {
+	if (!document.querySelector('.nav').contains(e.target) && !document.querySelector('.menu').contains(e.target)) {
+		// if user clicks outside menu close it
+		document.querySelector('.menu').classList.remove('active')
+	}
+})
+
+document.querySelector('.close-btn').addEventListener('click', () => {
+	document.querySelector('.menu').classList.remove('active')
+})
+
+let darkMode = localStorage.getItem('darkMode')
+
+const enableDarkMode = () => {
+	document.body.classList.add('darkmode')
+	localStorage.setItem('darkMode', 'enabled')
+	document.querySelector('.dark-mode-toggle .material-symbols-outlined').textContent = 'light_mode'
+}
+
+const disableDarkMode = () => {
+	document.body.classList.remove('darkmode')
+	localStorage.setItem('darkMode', null)
+	document.querySelector('.dark-mode-toggle .material-symbols-outlined').textContent = 'dark_mode'
+}
+if (darkMode === 'enabled') {
+	enableDarkMode()
+}
+
+document.querySelector('.dark-mode-toggle').addEventListener('click', () => {
+	darkMode = localStorage.getItem('darkMode')
+
+	if (darkMode !== 'enabled') {
+		enableDarkMode()
+	} else {
+		disableDarkMode()
+	}
+})
