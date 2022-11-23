5ad1fe988f034d4ee8c16ab83554ebac862978ef
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-23 14:44:58 +0100

    added working darkmode

--- multiple-hover-menus/script.js
+++ multiple-hover-menus/script.js
@@ -1,1 +1,26 @@
+let darkMode = localStorage.getItem('darkMode')
 
+const enableDarkMode = () => {
+	document.body.classList.add('darkmode')
+	localStorage.setItem('darkMode', 'enabled')
+	document.querySelector('.dark-mode-toggle .material-symbols-outlined').textContent = 'light_mode'
+}
+
+const disableDarkMode = () => {
+	document.body.classList.remove('darkmode')
+	localStorage.setItem('darkMode', null)
+	document.querySelector('.dark-mode-toggle .material-symbols-outlined').textContent = 'dark_mode'
+}
+if (darkMode === 'enabled') {
+	enableDarkMode()
+}
+
+document.querySelector('.dark-mode-toggle').addEventListener('click', () => {
+	darkMode = localStorage.getItem('darkMode')
+
+	if (darkMode !== 'enabled') {
+		enableDarkMode()
+	} else {
+		disableDarkMode()
+	}
+})
