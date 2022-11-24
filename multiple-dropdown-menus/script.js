282cf56f6cd3c0a304ac7150599904ce28adb443
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-24 14:57:56 +0100

    dark mode

--- multiple-dropdown-menus/script.js
+++ multiple-dropdown-menus/script.js
@@ -21,3 +21,32 @@ window.addEventListener('click', function (e) {
       })
 	}
 })
+
+
+// =================== Dark Mode ======================== //
+let darkMode = localStorage.getItem('darkMode')
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkMode', 'enabled')
+  document.querySelector('.dark-mode-toggle .material-symbols-outlined').textContent = 'light_mode'
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkMode', null)
+  document.querySelector('.dark-mode-toggle .material-symbols-outlined').textContent = 'dark_mode'
+}
+if (darkMode === 'enabled') {
+  enableDarkMode()
+}
+
+document.querySelector('.dark-mode-toggle').addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode')
+
+  if (darkMode !== 'enabled') {
+    enableDarkMode()
+  } else {
+    disableDarkMode()
+  }
+})
